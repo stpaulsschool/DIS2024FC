a4b5361f36023e048e8915dd086bdf2a193b119e
FletcherARC <27496@stpauls.qld.edu.au>
2023-01-27 12:42:41 +1000

    .

--- Unit1_Create_With_Code/mypython01.py
+++ Unit1_Create_With_Code/mypython01.py
@@ -5,11 +5,11 @@ line comment
 """
 # Input - asking user to input something and saving to a variable
 name = input("what is your name? ")
-age = input("what is your age? ")
+age = int(input("what is your age? "))
 
 # processing
 # python can calculate numbers
 age = 20 + age
  
-  # output
-  print("Hi," +name, "Your age in 20 years will be", age)
+# output
+print("Hi, " +name,", Your age in 20 years will be", age)
