fc3dd13c33322aec7a29dae24cb4e09c01ba49b4
FletcherARC <123520128+FletcherARC@users.noreply.github.com>
2023-01-27 12:16:30 +1000

    Create mypython01.py

---
+++ Unit1_Create_With_Code/mypython01.py
@@ -0,0 +1,15 @@
+# This is a comment
+"""
+This is a multi
+line comment
+"""
+# Input - asking user to input something and saving to a variable
+name = input("what is your name? ")
+age = input("what is your age? ")
+
+# processing
+# python can calculate numbers
+age = 20 + age
+ 
+  # output
+  print("Hi," +name, "Your age in 20 years will be", age)
