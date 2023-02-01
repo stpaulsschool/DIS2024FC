5869862cbc53c2d8c101466d0186509b45d912da
FletcherARC <123520128+FletcherARC@users.noreply.github.com>
2023-02-01 11:49:26 +1000

    Create wordsintitle.py

---
+++ wordsintitle.py
@@ -0,0 +1,6 @@
+test_string = input("What is the title of your book")
+#original string
+print ("The book name is : " + test_string)
+res = len(test_string.split())
+# total no of words
+print ("The number of words in this book tile are : " + str(res))
