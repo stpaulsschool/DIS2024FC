2628fd0c9f05d8322b8d66e10d5bdf6fb07bee43
FletcherARC <27496@stpauls.qld.edu.au>
2023-02-13 10:48:46 +1000

    .

---
+++ Unit1_Create_With_Code/Flask/00flask/app.py
@@ -0,0 +1,13 @@
+from flask import Flask  # First we imported the Flask class.
+
+app = Flask(__name__)  # next we create an instance of this class.
+
+
+@app.route('/')  # We then use the route() decorator to tell Flask what url should trigger our function.
+
+def hello_form():
+    return "<p>Hello, this is a form!<p>"
+    # the function returns the message we want to display in the user's browser.
+
+# if __name__ == '__main__':
+    # app.run(debug=True)
